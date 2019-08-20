39fc764046a3d81a426a0d8f697281a2e7820398
Gabriel <gabrieldarezzo@yahoo.com.br>
2019-08-20 20:03:12 -0300

    Show Gif

---
+++ main.js
@@ -0,0 +1,87 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, globalShortcut, window} = require('electron')
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+P', () => {    
+    // document.getElementById("gifbox").src = "assets/face-palm.gif";
+    mainWindow.show()
+    mainWindow.webContents.send('gifSource', 'assets/face-palm.gif')
+    
+    setTimeout(() => {
+      mainWindow.hide()
+    }, 2500) // <- Dinamicamente do Cadastro do GIF
+  })
+})
+
+
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+O', () => {
+    
+      // document.getElementById("gifbox").src = "assets/face-palm.gif";
+      mainWindow.show()
+      mainWindow.webContents.send('gifSource', 'assets/travolta.gif')
+      
+      setTimeout(() => {
+        mainWindow.hide()
+      }, 2500) // <- Dinamicamente do Cadastro do GIF
+  })
+})
+
+
+function createWindow () {
+  
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    // width: 800,
+    // height: 600,
+    show: false,  // TODO
+    transparent:true,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+   // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+
+
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
